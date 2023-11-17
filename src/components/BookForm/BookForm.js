d3d63a10f4740af876e5a1d042f5a66682dd1349
Adrian Manczak <manczakadrian@gmail.com>
2023-11-17 23:47:21 +0100

    BookFormandBookList_comunication

--- src/components/BookForm/BookForm.js
+++ src/components/BookForm/BookForm.js
@@ -1,6 +1,23 @@
-const BookForm=()=>{
-    return(
-        <h2>BookForm</h2>
+import { useState } from "react";
+
+const BookForm = ({addBook}) => {
+
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        addBook({title: title, author:author});
+        setTitle('');
+        setAuthor('');
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            Title:  <input type="text" value={title} onChange={e => setTitle(e.target.value)} />
+            Author:  <input type="text" value={author} onChange={e => setAuthor(e.target.value)} />
+            <button>Add Book</button>
+        </form>
     );
 };
 export default BookForm;
--- src/components/BooksList/BooksList.js
+++ src/components/BooksList/BooksList.js
@@ -1,6 +1,11 @@
-const BookList =()=>{
-    return(
-        <h2>BooksList</h2>
+
+
+const BookList =({books, removeBook})=>{
+    return (
+        <ul>
+        {books.map(book=><li key={book.id}>{book.title} by {book.author} 
+        <button onClick={()=>removeBook(book.id)}>Remove</button></li>)}
+        </ul>
     );
 };
 export default BookList;
